b5ad6cdbb444bfcd26e005e0e2b0d27fa28144ab
Arleigh Dickerson <arleighdickerson@gmail.com>
2014-09-01 12:58:10 -0400

    added naive implementation in Wilkinson's

--- Wilkinsons.py
+++ Wilkinsons.py
@@ -10,13 +10,24 @@ def clenshaw(x, index=1, accum=1):
     else :
         return clenshaw(x, index + 1, accum * (x - index))
     
+#ensures that roots evaluate to zero (low hanging fruit)
 def clenshawTest() :
     for x in range(1,N+1):
         assert clenshaw(x) == 0
 
 #construct our (wilkinson's) polynomial instance
+#verified output on wikipedia
 def buildPolynomial():
     polynomialValue = np.poly1d([1])
     for i in range(1,N+1) :
         polynomialValue *= np.poly1d([1,(i / -1)])
     return polynomialValue
+
+#param x, value to be evald
+#param a, list of polynomial coeffs. DEGREES ARE ASCENDING
+#verified output with mathematica
+def naive(x,a):
+    accum = 0
+    for k in range(len(a)):
+        accum += a[k] * (x ** k)
+    return accum
