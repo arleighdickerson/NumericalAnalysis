88b16598febee121109b1998ad7d1d22b56afc6e
Arleigh Dickerson <arleighdickerson@gmail.com>
2014-09-01 15:17:49 -0400

    plotting

--- Wilkinsons.py
+++ Wilkinsons.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pylab as pl
 
 N = 20
 
@@ -10,27 +11,36 @@ def clenshaw(x, index=1, accum=1):
     else :
         return clenshaw(x, index + 1, accum * (x - index))
     
-#ensures that roots evaluate to zero (low hanging fruit)
+# ensures that roots evaluate to zero (low hanging fruit)
 def clenshawTest() :
-    for x in range(1,N+1):
+    for x in range(1, N + 1):
         assert clenshaw(x) == 0
 
-#construct our (wilkinson's) polynomial instance
-#verified output on wikipedia
+# construct our (wilkinson's) polynomial instance
+# verified output on wikipedia
 def buildPolynomial():
     polynomialValue = np.poly1d([1])
-    for i in range(1,N+1) :
-        polynomialValue *= np.poly1d([1,(i / -1)])
+    for i in range(1, N + 1) :
+        polynomialValue *= np.poly1d([1, (i / -1)])
     return polynomialValue
 
-#param x, value to be evald
-#param a, list of polynomial coeffs. DEGREES ARE ASCENDING
-#verified output with mathematica
-def naive(x,a):
+def coefficientList():
+    coefficients = buildPolynomial().coeffs.tolist()
+    coefficients.reverse()
+    return coefficients
+
+# param x, value to be evald
+# param a, list of polynomial coeffs. DEGREES ARE ASCENDING
+# verified output with mathematica
+def naive(x, a=coefficientList()):
     accum = 0
     for k in range(len(a)):
         accum += a[k] * (x ** k)
     return accum
+
+def testRoots(f):
+    for x in range(1,N+1):
+        assert f(x) == 0
 
 def makeClenshawData():
     results = {}
@@ -47,9 +57,12 @@ def makeNumpyPolyData():
     return results
 
 def makeNaiveData():
-    coefficients = buildPolynomial().coeffs.tolist()
-    coefficients.reverse()
+    coefficients = coefficientList()
     results = {}
     for x in inputValues:
         results[x] = naive(x, coefficients)
     return results
+
+def showPlot(data):
+    pl.plot(data.keys(), data.values(), "ro")
+    pl.show()
