3126b7ee1ffcf8a887ce52f2ba485d3a69e598e1
Arleigh Dickerson <arleighdickerson@gmail.com>
2014-09-04 10:23:01 -0400

    hardcoded coefficients for now

--- Wilkinsons.py
+++ Wilkinsons.py
@@ -1,9 +1,9 @@
+import numpy as np
+from matplotlib import pyplot
 '''
 @author: Arleigh Dickerson
 '''
 
-import numpy as np
-from matplotlib import pyplot
 i = 1
 N = 20
 
@@ -30,7 +30,7 @@ def clenshaw(x, index=i, accum=1):
     @param index: the index of the current iteration.  Clients call with default
         value.
     @param accum: the accumulator. Clients call with default value.
-    @return: the clenshaw evaluation at x for a wilkinson's polynomial 
+    @return: the clenshaw evaluation at x for a numpyPolynomial's polynomial 
         parameterized by i and N
     '''
     if index > N:
@@ -38,27 +38,33 @@ def clenshaw(x, index=i, accum=1):
     else:
         return clenshaw(x, index + 1, accum * (x - index))
     
-def wilkinson():
-    '''
-    @return a poly1d instance representing the Wilkinson's polynomial described 
-        by i and N.
-    '''
-    return reduce(lambda b0, b1: b0 * b1,
-                  map(lambda n:np.poly1d([1, abs(n) * -1]), roots()))
-
-def coefficients(ascending=True):
-    '''
-    @param ascending: 
-        True if the returned list should be sorted with degrees ascending, 
-        False if the returned list should be sorted with degrees descending.
-    @type ascending: bool
-    @return a list of coefficients for the Wilkinson's polynomial described by
-        i and N.
-    '''
-    coefficients = wilkinson().coeffs.tolist()
-    if(ascending):
-        coefficients.reverse()
-    return coefficients
+def makePolynomial() :
+    a = coefficients()
+    a.reverse()
+    return np.poly1d(a)
+    
+def coefficients():
+    return [2432902008176640000,
+            - 8752948036761600000,
+            13803759753640704000,
+            - 12870931245150988800,
+            8037811822645051776,
+            - 3599979517947607200,
+            1206647803780373360,
+            - 311333643161390640,
+            63030812099294896,
+            - 10142299865511450,
+            1307535010540395,
+            - 135585182899530,
+            11310276995381,
+            - 756111184500,
+            40171771630,
+            - 1672280820,
+            53327946,
+            - 1256850,
+            20615,
+            - 210,
+            1]
 
 def naive(x, a=coefficients()):
     '''
