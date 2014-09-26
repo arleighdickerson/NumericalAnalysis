89eb31f5be5b2591829d86a61aa20780c18d3146
Arleigh Dickerson <arleighdickerson@frugalu.com>
2014-09-26 12:07:03 -0400

    Update RootFinders.py

--- RootFinders.py
+++ RootFinders.py
@@ -1,51 +1,69 @@
 '''
-@author: Arleigh Dickerson
+@author: Professor
 '''
+# import RootFinders as R
 
-def bisection(f, a, b, tol=1e-9):
-    from numpy import ceil, log2
-    fa, fb = f(a), f(b)
-    N = int(ceil(log2((b + a) / tol)))
-    x = []
-    for i in range(N):
-        c = (b - a) / 2
-        x.append(c)
-        fc = f(c)
-        if fa * fc < 0:  # x* is in [a,c]
-            b, fb = c, fc
-        else:
-            a, fa = c, fc
-    x.append((b + a) / 2)
-    return x
+from numpy import ceil, log2
+from math import fabs
 
-def newton(f, fPrime, x0, tol=1e-16, maximumIterations=50):
-    deltax, fOfx = 100, 100
-    counter = 0
-    xa = [x0]
-    x = x0
-    
-    while (fabs(deltax) > tol or fabs(fOfx) > tol) and counter <= maximumIterations:
-        fOfx, fPrimeOfx = f(x), fPrime(x)
-        deltax = -fOfx / fPrimeOfx
-        x += deltax
-        counter += 1
-        xa.append(x)
+# -------------------------------------------
 
-    return xa
+def bisection(f,a,b,tol=1e-9):
+	fa, fb = f(a), f(b)
+	N = int( ceil( log2((b-a)/tol ) ) )
+	
+	x = []  # empty list
+	
+	for i in range(N):
+		c = (b+a)/2
+		x.append(c)
+		fc = f(c)
+		if fa*fc < 0:  # x* is in [a,c]
+			b, fb = c, fc
+		else:
+			a, fa = c, fc
+			
+	x.append((b+a)/2)
+	return x
+	
+# ----------------------------------------------
 
-def newtonSystem(f, J, x0, tol=1e-10, maxIt=50):
-    N = len(x0)
-    dx, fx = ones((N,)), ones((N,))
-    count = 0
-    xa = []
-    xa.append(x0)
-    x = x0
-    
-    while norm(dx, inf) > tol or norm(fx, inf > tol) and count <= maxIt:
-        fx, fpx = f(x), J(x)
-        dx = solve(fpx, -fx)
-        x += dx
-        count += 1
-        xa.append(x)
-    
-    return xa
+def newton(f,fp,x0,tol=1e-10,maxIt=50):
+	dx, fx = 100, 100
+	iter = 0
+	xa = []
+	xa.append(x0)
+	x = x0
+	
+	while fabs(dx)>tol or fabs(fx)>tol and iter<=maxIt:
+		fx, fpx = f(x), fp(x)
+		dx = -fx/fpx
+		print(dx)
+		x += dx
+		iter += 1
+		xa.append(x)
+		
+	return xa
+	
+# ----------------------------------------------
+
+from pylab import *
+
+def newtonSystem(f,J,x0,tol=1e-10,maxIt=50):
+	
+	N = len(x0)
+	dx, fx = ones((N,)), ones((N,))
+	iter = 0
+	xa = []
+	xa.append(x0)
+	x = x0
+	
+	while norm(dx,inf)>tol or norm(fx,inf)>tol and iter<=maxIt:
+		fx, fpx = f(x), J(x)
+#		dx = -fx/fpx
+		dx = solve(fpx,-fx)
+		x += dx
+		iter += 1
+		xa.append(x)
+		
+	return xa
